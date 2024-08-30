4320f10dd0e6881352d00ebeae9b54fa6119dc68
cepix1234 <bocepek027@gmail.com>
2024-08-30 23:36:33 +0200

    Added sh publish script

---
+++ working/content/cepixConsole/publish.sh
@@ -0,0 +1,25 @@
+dotnet publish ./app/cepixConsole.App -r linux-x64 -p:PublishSingleFile=true --self-contained true
+dotnet publish ./app/cepixConsole.App -r win-x64 -p:PublishSingleFile=true --self-contained true
+
+ReleasePath = "./Release"
+ReleaseWinPath = "$ReleasePath/Windows"
+ReleaseLinuxPath = "$ReleasePath/Linux"
+
+if [ ! -d "$ReleasePath"]; then
+  mkdir "$ReleasePath"
+  mkdir "$ReleaseWinPath"
+  mkdir "$ReleaseLinuxPath"
+fi
+
+if [ ! -d "$ReleaseWinPath"]; then
+  mkdir "$ReleaseWinPath"
+fi
+
+if [ ! -d "$ReleaseLinuxPath"]; then
+  mkdir "$ReleaseLinuxPath"
+fi 
+
+cp ./app/cepixConsole.App/bin/Release/net8.0/win-x64/publish/cepixConsole.App.exe "${ReleaseWinPath}/cepixConsole.exe"
+cp ./app/cepixConsole.App/bin/Release/net8.0/win-x64/publish/appsettings.json "${ReleaseWinPath}/appsettings.json"
+cp ./app/cepixConsole.App/bin/Release/net8.0/linux-x64/publish/cepixConsole.App "${ReleaseLinuxPath}/cepixConsole"
+cp ./app/cepixConsole.App/bin/Release/net8.0/linux-x64/publish/appsettings.json "${ReleaseLinuxPath}/appsettings.json"
